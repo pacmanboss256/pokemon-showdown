d634572e593a4fa77e6c9c58e94dd79b57dd1c6e
pacmanboss256 <pacmanboss972@gmail.com>
2021-03-09 09:30:27 -0600

    no

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -3263,7 +3263,7 @@ export const Pokedex: { [speciesid: string]: SpeciesData } = {
 		types: ["Fire"],
 		genderRatio: {M: 0.875, F: 0.125},
 		baseStats: {hp: 78, atk: 84, def: 78, spa: 109, spd: 85, spe: 100},
-		abilities: {0: "Blaze", H: "Flash Fire"},
+		abilities: {0: "Blaze", 1: "Blazing Soul", H: "Flash Fire"},
 		heightm: 1.7,
 		weightkg: 79.5,
 		color: "Yellow",
