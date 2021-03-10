9c263428dd0de96b36bebaa21e0ee6f20cd7fc10
pacmanboss256 <pacmanboss972@gmail.com>
2021-03-10 15:39:07 -0600

    misty explosion halves defense, all stat changes for existing mons from 2.1=>2.2 are implemented

--- sim/battle.ts
+++ sim/battle.ts
@@ -2178,7 +2178,10 @@ export class Battle {
 		attack = this.runEvent('Modify' + statTable[attackStat], attacker, defender, move, attack);
 		defense = this.runEvent('Modify' + statTable[defenseStat], defender, attacker, move, defense);
 
-		if (this.gen <= 4 && ['explosion', 'selfdestruct'].includes(move.id) && defenseStat === 'def') {
+		if (((this.gen <= 4) || (this.gen === 8)) && ['explosion', 'selfdestruct'].includes(move.id) && defenseStat === 'def') {
+			defense = this.clampIntRange(Math.floor(defense / 2), 1);
+		}
+		if (this.gen === 8 && ['mistyexplosion'].includes(move.id) && defenseStat === 'spd') {
 			defense = this.clampIntRange(Math.floor(defense / 2), 1);
 		}
 
