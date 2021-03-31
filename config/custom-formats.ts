250ec9aa66c975969e80c4d243bc350117a2206e
Karthik99999 <bandagondak0217@gmail.com>
2021-03-30 22:38:03 -0400

    add uu format

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -5,25 +5,11 @@ export const Formats: FormatList = [
 		section: "National Dex",
 	},
 	{
-		name: "[Gen 8] National Dex RRDL",
-		mod: 'gen8',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Sleep Clause Mod', 'Dynamax Clause', 'CFZ Clause', 'Z-Move Clause'],
-		banlist: [
-			// mons
-			'Alakazam-Mega', 'Arceus', 'Blaziken-Mega', 'Calyrex-Ice', 'Calyrex-Shadow', 'Chandelure', 'Darkrai', 'Darmanitan-Galar', 'Deoxys-Attack', 'Deoxys-Base', 'Deoxys-Speed', 'Dialga', 'Dragapult', 'Eternatus', 'Genesect', 'Gengar-Mega', 'Giratina', 'Giratina-Origin', 'Greninja-Ash', 'Groudon', 'Ho-Oh', 'Kangaskhan-Mega', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Latias-Mega', 'Latios-Mega', 'Landorus-Base', 'Lucario-Mega', 'Lugia', 'Lunala', 'Magearna', 'Marshadow', 'Metagross-Mega', 'Mewtwo', 'Naganadel', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Pheromosa', 'Rayquaza', 'Reshiram', 'Salamence-Mega', 'Shaymin-Sky', 'Silvally', 'Solgaleo', 'Spectrier', 'Urshifu-Base', 'Xerneas', 'Yveltal', 'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Zygarde-Complete',
-			// abilities
-			'Moody', 'Power Construct', 'Battle Bond',
-			// moves
-			'Swagger', 'Baton Pass', 'Dark Hole', 'Misty Explosion', 'Explosion', 'Self-Destruct',
-		],
-	},
-
-	{
 		name: "[Gen 8] National Dex RR OU",
 		mod: 'gen8',
 		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Sleep Clause Mod', 'Dynamax Clause', 'CFZ Clause', 'Z-Move Clause'],
 		banlist: [
-			// mons
+			// ubers
 			'Alakazam-Mega', 'Arceus', 'Blastoise-Mega', 'Blaziken-Mega', 'Calyrex-Ice', 'Calyrex-Shadow', 'Darkrai', 'Deoxys-Attack', 'Deoxys-Base', 'Deoxys-Speed', 'Dialga', 'Eternatus', 'Genesect', 'Gengar-Mega', 'Giratina', 'Giratina-Origin', 'Groudon', 'Ho-Oh', 'Kangaskhan-Mega', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Landorus-Base', 'Lucario-Mega', 'Lugia', 'Lunala', 'Marshadow', 'Metagross-Mega', 'Mewtwo', 'Naganadel', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Pheromosa', 'Rayquaza', 'Reshiram', 'Salamence-Mega', 'Shaymin-Sky', 'Solgaleo', 'Tapu Koko', 'Xerneas', 'Yveltal', 'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Zygarde-Complete',
 			// abilities
 			'Moody', 'Power Construct', 'Shadow Tag', 'Arena Trap',
@@ -36,12 +22,27 @@ export const Formats: FormatList = [
 		mod: 'gen8',
 		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Sleep Clause Mod', 'Dynamax Clause', 'CFZ Clause', 'Z-Move Clause'],
 		banlist: [
-			// mons
+			// AG
 			'Rayquaza-Mega', 'Zacian-Crowned',
 			// moves
 			'Baton Pass', 'Dark Hole',
 			// abilities
 			'Moody',
+		],
+	},
+	{
+		name: "[Gen 8] National Dex RR UU",
+		mod: 'gen8',
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Sleep Clause Mod', 'Dynamax Clause', 'CFZ Clause', 'Z-Move Clause'],
+		banlist: [
+			// ou
+			'Magearna', 'Spectrier', 'Cinderace', 'Tornadus-Therian', 'Landorus-Therian', 'Zapdos', 'Gliscor', 'Heatran', 'Dragapult', 'Tapu Bulu', 'Tapu Fini', 'Tapu Lele', 'Clefable', 'Kartana', 'Hydreigon', 'Lopunny-Mega', 'Ferrothorn', 'Tangrowth', 'Magnezone', 'Dracovish', 'Centiskorch-Mega', 'Corviknight', 'Darmanitan-Galar', 'Latias-Mega', 'Garchomp', 'Garchomp-Mega', 'Excadrill', 'Tyranitar-Mega', 'Infernape', 'Blissey', 'Toxtricity-Mega', 'Greninja', 'Greninja-Ash', 'Medicham-Mega', 'Swampert-Mega', 'Pelipper', 'Regieleki', 'Rotom-Heat', 'Slowbro', 'Slowbro-Mega', 'Volcarona', 'Mawile-Mega', 'Moltres-Galar',
+			// uubl (add mons after nduubl)
+			'nduubl',
+			// abilities
+			'Moody', 'Power Construct', 'Shadow Tag', 'Arena Trap',
+			// moves
+			'Swagger', 'Baton Pass', 'Dark Hole', 'Misty Explosion',
 		],
 	},
 	{
